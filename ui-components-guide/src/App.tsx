71140fe2f292388f85a6731f1ee81b30c9615dc1
JAESOL SHIN <ysys143@naver.com>
2025-07-03 03:45:34 +0900

    Fix GitHub Pages routing: switch to HashRouter and add SPA support

--- ui-components-guide/src/App.tsx
+++ ui-components-guide/src/App.tsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import UserManagement from './pages/UserManagement';
